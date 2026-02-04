3671c2a1ed84732099b2d194f120754c94abaa14
Justin E <justin.elak@gmail.com>
2026-02-04 16:29:26 -0500

    add main.py and class structure

---
+++ main.py
@@ -0,0 +1,12 @@
+class Course:
+    def __init__(self, course_code: str, credits: int, students: list[Student]):
+        pass
+
+    def add_student(self, student: Student):
+        pass
+
+    def get_student_count(self) -> int:
+        pass
+
+class Student:
+    pass
