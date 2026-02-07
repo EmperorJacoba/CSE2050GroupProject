f07269c0d792d1218dc0146bb9cd0f962fadf6a5
Justin Elak <justin.elak@gmail.com>
2026-02-07 18:12:19 -0500

    Add test cases for Student and Course classes

--- pyfiles/holistic_program_testcases.py
+++ pyfiles/holistic_program_testcases.py
@@ -1,7 +1,21 @@
 import unittest
+from student_class import Student
+from course import Course
+from university import University
 
-class MyTestCase(unittest.TestCase):
-    pass
+
+class TestCourse(unittest.TestCase):
+    def test_init(self):
+        course1 = Course("CSE2050", 3,[])
+        self.assertEqual(course1.course_code, "CSE2050")
+        self.assertEqual(course1.credits, 3)
+        self.assertEqual(course1.students, [])
+
+    def test_add_student(self):
+        course1 = Course("CSE2050", 3,[])
+        student1 = Student("STU74823", "Justin", {})
+        course1.add_student(student1)
+        self.assertEqual(course1.students, [student1])
 
 
 if __name__ == '__main__':
