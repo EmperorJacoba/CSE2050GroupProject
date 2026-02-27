af4ec921f3d07523082d10358f2dff35594bc141
Justin E <justin.elak@gmail.com>
2026-02-27 18:40:51 -0500

    Add main.py

---
+++ pyfiles/main.py
@@ -0,0 +1,47 @@
+import sys
+import csv_parser
+from university import University
+
+def print_menu():
+    print("a: Get list of students in course")
+    print("b: Get student GPA")
+    print("c : Print student course data")
+    print("d : Print common students in two courses")
+    print("e: Print mean/med/mode for a course")
+    print("q: Quit")
+
+def prompt_user():
+    print_menu()
+    user_input = input("Select a menu option: ")
+    while user_input != "q":
+        match user_input:
+            case "a":
+                pass
+            case "b":
+                pass
+            case "c":
+                pass
+            case "d":
+                pass
+            case "e":
+                pass
+            case _:
+                print("Invalid Input, try again.")
+                print_menu()
+
+course_cat = sys.argv[1]
+student_data = sys.argv[2]
+
+university = University()
+csv_parser.read_course_data(university, course_cat)
+csv_parser.read_uni_data(university, student_data)
+
+print(f"Median GPA: {university.get_median_gpa()}")
+print(f"Mean GPA: {university.get_mean_gpa()}")
+
+
+course_code = input("Input a course to show the student list for:")
+university.print_students_in_course(course_code)
+
+student_id = input("Input the student id a student you would like the see the GPA and course info of:")
+print(f"GPA: {university.get_student(student_id).calculate_gpa()}\n{university.get_student(student_id).get_course_info()}")
