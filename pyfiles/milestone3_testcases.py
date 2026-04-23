c6e2e9cfa4740caf00cab069a048c39237a8ee86
Emperor <emperorjacoba@gmail.com>
2026-04-23 16:35:05 -0400

    Download prereqs, make testcase/sorting algs skeleton, add prereq checking in course

---
+++ pyfiles/milestone3_testcases.py
@@ -0,0 +1,34 @@
+import unittest
+from hash import HashMap
+from course import Course
+
+class TestHashMap(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_init(self):
+        pass
+
+    def test_collision_handling(self):
+        pass
+
+    def test_rehashing(self):
+        pass
+
+class TestEnrollments(unittest.TestCase):
+    def test_check_for_prerequisites(self):
+        pass
+
+class TestSortingAlgorithms(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_merge_sort(self):
+        # test course correctly sorted by id, name, date
+        # use setUp/test procedure from m2 testcases for other algs
+        pass
+
+    def test_quick_sort(self):
+        # test course correctly sorted by id, name, date
+        # use setUp/test procedure from m2 testcases for other algs
+        pass
